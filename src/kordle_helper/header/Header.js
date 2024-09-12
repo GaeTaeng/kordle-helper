b497821f5f0fc05b774192e15e40efd11bbd9c00
Kim TaeHun <nuckly60@gmail.com>
2024-09-12 18:01:16 +0900

    카카오톡 오픈채팅 추가

--- src/kordle_helper/header/Header.js
+++ src/kordle_helper/header/Header.js
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <header style={{marginTop:"20px"}}>
-        문의사항 : nuckly60@gmail.com
+        문의사항 : nuckly60@gmail.com / <a href="https://open.kakao.com/o/gdYCzqOg"  target='_blank'>카카오톡</a>
         </header>
     )
 }
