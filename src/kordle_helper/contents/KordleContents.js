c6c483052bf2eaef412cb471603f725bcd60517d
Kim TaeHun <nuckly60@gmail.com>
2024-09-13 13:23:50 +0900

    css 수정과 rnb 미사용단어 추가

--- src/kordle_helper/contents/KordleContents.js
+++ src/kordle_helper/contents/KordleContents.js
@@ -8,6 +8,7 @@ function KordleContents({type}) {
     const [answerList, setAnswerList] = useState( ["","","","","","","","","","","","",]);
     const [exampleList, setExampleList] = useState([init_1,init,init,init,init,init,init,init,init,init,init,init] )
     const [warningList, setWarningList] = useState([["",],["",],["",],["",],["",],["",],["",],["",],["",],["",],["",],["",]]);
+    const [notUseList, setNotUseList] = useState( [["","","","","","","","","","","","",], ["","","","","","","","","","","","",]]);
     
 
     useEffect(() => {
@@ -130,55 +131,143 @@ function KordleContents({type}) {
         }))
 
     }
+
+
+
+    const handleKeyDownNotUseInput = (e, idx) => {
+        
+        if( e.keyCode === 13 ) {
+            handleInputNotUse(e.target.value, idx)
+            e.target.value= ""
+        }
+
+    }
+
+    const handleBlurNotUseInput = (e, idx) => {
+        console.log("e : ", e)
+        if(  e.type === 'blur') {
+            handleInputNotUse(e.target.value, idx)
+            e.target.value= ""
+        }
+
+    }
+    const handleInputNotUse = (text, idx) => {
+        
+        if(text.length > 1) return;
+        if(answerList[idx]) return;
+
+        text = replaceExceptKorean(convertEnglishToKorean(text))
+        if(notUseList[idx].includes(text)) return;
+        
+        
+        setNotUseList(notUseList.map((notUse, _idx) => {
+            if(_idx === idx) {
+                return [...notUse.filter(item => item !== ""), text]
+            }else {
+                return notUse
+            }
+            
+        }))
+    }
+
+    const handleDeleteNotUseItem = (idx1, idx2) => {
+        setNotUseList(notUseList.map((notUse, _idx) => {
+            if(_idx === idx1) {
+                if(notUse.length <= 1)  {
+                    return notUse.map((item, _idx2) => {
+                        if(_idx2 !== idx2) return item;
+                        return ""
+                    })
+                }else {
+                    return notUse.filter((item, _idx2) => {
+                        return _idx2 !== idx2;
+                    })
+                }
+                
+
+            }
+                return notUse
+            
+        }))
+
+    }
     console.log("Answer : ", answerList)
     return (
-        <div>
-            <div className="section">
-                <div className="answerInputContainer" >
-                    {answerList.map((item, idx) => {
-                        return <div key={`answerInputCase_${idx}`} className="answerInputCase">
-                                <div className="answerInputIdx" >{idx}</div>
-                                <input className="answerInput" id={`answerInput_${idx}`} style={item && item === "" ? {} : {cursor:"pointer"}} pattern="[ㄱ-ㅎ]" onClick={() => answerInit(idx)} onKeyDown={(e, c) => {onChangeAnswerItem(e, c, idx)}} value={item} readOnly={item}/>
-                            </div>
-                    })}
+        <div className="kordleContents">
+            <div className="leftContents">
+                <div className="section">
+                    <div className="answerInputContainer" >
+                        {answerList.map((item, idx) => {
+                            return <div key={`answerInputCase_${idx}` } className="answerInputCase word">
+                                    <div className="answerInputIdx" >{idx}</div>
+                                    <input className="answerInput" id={`answerInput_${idx}`} style={item && item === "" ? {} : {cursor:"pointer"}} pattern="[ㄱ-ㅎ]" onClick={() => answerInit(idx)} onKeyDown={(e, c) => {onChangeAnswerItem(e, c, idx)}} value={item} readOnly={item}/>
+                                </div>
+                        })}
 
+                    </div>
                 </div>
-            </div>
 
 
-            <div className="section">
-                <div className="exampleListContainer">
+                <div className="section">
+                    <div className="exampleListContainer">
 
-                {exampleList.map((example, idx) => {
-                        return <div key={`exampleListCase_${idx}`} className="exampleListCase">
-                                {example.filter(item => item !== answerList[idx] && !warningList[idx].includes(item)).map((item, idx2) => {
-                                    return <div key={`exmapleItem_${idx2}`} className="exmapleItem" style={{height : `calc(100%/${example.filter(item => item !== answerList[idx] && !warningList[idx].includes(item)).length})`}}
-                                    onClick={() => {handleSelectedExampleItem(item, idx)}}>
-                                            {item}
-                                        </div>
-                                })}
-                            </div>
-                    })}
-                </div>
-            </div>
-
-            <div className="section">
-                흰색 영역에 입력 후 엔터
-                <div className="warningListContainer">
-                
-                    {warningList.map((warning, idx) => {
-                            return <div key={`warningListCase_${idx}`} className="warningListCase">
-                                <input key={`warningItem_${idx}`} className="warningItemInput" onKeyDown={(e) => {handleKeyDownWarningInput(e, idx)}} onBlur={(e) => {handleBlurWarningInput(e, idx)}}/>
-                                    {warning.map((item, idx2) => {
-                                        return <div key={`warningItem_${idx2}`} className="warningItem" onClick={() => {handleDeleteWarningItem(idx, idx2)}}>
+                    {exampleList.map((example, idx) => {
+                            return <div key={`exampleListCase_${idx}`} className="exampleListCase word">
+                                    {example.filter(item => item !== answerList[idx] && !warningList[idx].includes(item)).map((item, idx2) => {
+                                        return <div key={`exmapleItem_${idx2}`} className="exmapleItem" style={{height : `calc(100%/${example.filter(item => item !== answerList[idx] && !warningList[idx].includes(item)).length})`}}
+                                        onClick={() => {handleSelectedExampleItem(item, idx)}}>
                                                 {item}
                                             </div>
                                     })}
                                 </div>
                         })}
+                    </div>
+                </div>
+
+                <div className="section">
+                    <div>흰색 영역에 입력 후 엔터</div> 
+                    <div>(해당칸에서는 사용하지 않지만 다른칸에서 사용하는 단어)</div>
+                    <div className="warningListContainer">
+                    
+                        {warningList.map((warning, idx) => {
+                                    console.log("warning : ", warning)
+                                return <div key={`warningListCase_${idx}`} className="warningListCase word">
+                                    <input key={`warningItem_${idx}`} className="warningItemInput" onKeyDown={(e) => {handleKeyDownWarningInput(e, idx)}} onBlur={(e) => {handleBlurWarningInput(e, idx)}}/>
+                                        {warning.map((item, idx2) => {
+                                            return <div key={`warningItem_${idx2}`} className="warningItem" onClick={() => {handleDeleteWarningItem(idx, idx2)}}>
+                                                    {item}
+                                                </div>
+                                        })}
+                                    </div>
+                            })}
+                    </div>
                 </div>
             </div>
+
+            <div className="rightContents">
+                
+                <div className="section">
+                    <div>흰색 영역에 입력 후 엔터</div> 
+                    <div>(더이상 사용하지 않을 자음/모음들을 입력)</div>
+                    <div className="notUseListContainer">
+                    
+                        {notUseList.map((notUse, idx) => {
+                            console.log("notUse : ", notUse)
+                                return <div key={`notUseListCase_${idx}`} className="notUseListCase word">
+                                    <input key={`notUseItem_${idx}`} className="notUseItemInput" onKeyDown={(e) => {handleKeyDownNotUseInput(e, idx)}} onBlur={(e) => {handleBlurNotUseInput(e, idx)}}/>
+                                        {notUse.map((item, idx2) => {
+                                            return <div key={`notUseItem_${idx2}`} className="notUseItem" onClick={() => {handleDeleteNotUseItem(idx, idx2)}}>
+                                                    {item}
+                                                </div>
+                                        })}
+                                    </div>
+                            })}
+                    </div>
+                </div>
+            </div>
+
         </div>
+        
     )
     
 }
