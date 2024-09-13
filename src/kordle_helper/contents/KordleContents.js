361c069a3daa2486941869074c5e8b4813768cd1
Kim TaeHun <nuckly60@gmail.com>
2024-09-13 15:05:24 +0900

    notice 추가

--- src/kordle_helper/contents/KordleContents.js
+++ src/kordle_helper/contents/KordleContents.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./KordleContents.css"
+import "./popup.css"
 import { GAME_TYPE } from "./kordleConst";
 const init_1 = ["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
 const init = ["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "ㅏ", "ㅑ", "ㅓ", "ㅕ", "ㅗ", "ㅛ", "ㅜ", "ㅠ", "ㅡ", "ㅣ"];
@@ -11,6 +12,7 @@ function KordleContents({type}) {
     const [notUseList, setNotUseList] = useState( [["",],["",]]);
     
 
+    const [isOpenNotice, setIsOpenNotice] = useState(true)
     useEffect(() => {
         console.log("type : ", type)
         if(String(type) === String(GAME_TYPE.KORDLE)) {
@@ -192,8 +194,31 @@ function KordleContents({type}) {
 
     }
     console.log("Answer : ", answerList)
+
+    const handleCloseNoticePopup = () => {
+        setIsOpenNotice(false);
+    }
     return (
         <div className="kordleContents">
+            <dialog open={isOpenNotice}>
+                <h1>해당 페이지는 꼬들/꼬오오오오들 문제풀이를 도와주는 헬프 페이지입니다.</h1>
+                <p>꼬들/꼬오오오오들은 한글 버전의 숫자야구게임/아나그램 입니다. </p>
+                <p>확정된 글자를 입력(초록칸/숫자야구에임의 strike) 하고 </p> 
+                <p>포함되지만 칸이 다른 글자(노란칸/숫자야구게임의 ball)를 입력하고 </p> 
+                <p>사용되지 않는 글자(회색칸/숫자야구게임의 out)을 입력하여 </p> 
+                <p>남은 자/모를 확인하며 단어조합에 약간이나마 도움을 주기위해 작성된 페이지입니다.</p> 
+
+
+                <p>혹 사용간 불편한 사항이나 건의사항은 아래 메일 또는 카카오톡 오픈채팅을 통해 전달주시면 감사합니다.</p>
+                <p>E-Mail : nuckly60@gmail.com </p>
+                <p>KAKAO TALK :  <a href="https://open.kakao.com/o/gdYCzqOg"  target='_blank'>카카오톡</a></p>
+                <br />
+                <p>그럼 즐거운 게임되세요! </p>
+                <p>감사합니다 </p>
+
+                <button className="noticeButton" onClick={handleCloseNoticePopup}>닫기</button>
+                
+            </dialog>
             <div className="leftContents">
                 <div className="section">
                     <div className="answerInputContainer" >
@@ -265,6 +290,8 @@ function KordleContents({type}) {
                     </div>
                 </div>
             </div>
+
+
 
         </div>
         
