3a0857e60b1443f0136da6690099bb1590e466d6
Kim TaeHun <nuckly60@gmail.com>
2024-09-16 22:01:58 +0900

    example 추가

--- src/kordle_helper/contents/KordleContents/ExampleListContainer/ExampleListContainer.js
+++ src/kordle_helper/contents/KordleContents/ExampleListContainer/ExampleListContainer.js
@@ -8,8 +8,8 @@ function ExampleListContainer({exampleList, answerList, warningList, notUseList,
 
                     {exampleList.map((example, idx) => {
                             return <div key={`exampleListCase_${idx}`} className="exampleListCase word">
-                                    {example.filter(item => item !== answerList[idx] && !warningList[idx].includes(item) && !notUseList[0].includes(item) && !notUseList[1].includes(item)).map((item, idx2) => {
-                                        return <div key={`exmapleItem_${idx2}`} className="exmapleItemBox" style={{height : `calc(100%/${example.filter(item => item !== answerList[idx] && !warningList[idx].includes(item) && !notUseList[0].includes(item) && !notUseList[1].includes(item)).length})`}}>
+                                    {example.filter(item => answerList[idx] !== "" ? item === answerList[idx] : item !== answerList[idx] && !warningList[idx].includes(item) && !notUseList[0].includes(item) && !notUseList[1].includes(item)).map((item, idx2) => {
+                                        return <div key={`exmapleItem_${idx2}`} className="exmapleItemBox" style={{height : `calc(100%/${example.filter(item =>  answerList[idx] !== "" ? item === answerList[idx] : item !== answerList[idx] && !warningList[idx].includes(item) && !notUseList[0].includes(item) && !notUseList[1].includes(item)).length})`}}>
                                             <div className="exmapleItem"  onClick={() => {handleSelectedExampleItem(item, idx)}}>
                                                 {item}
                                             </div>
