ab2703e6531752ddee7cfa37eadd848b84702239
Kim TaeHun <nuckly60@gmail.com>
2024-10-14 23:17:04 +0900

    ads re

--- src/kordle_helper/KordleHelperMain.js
+++ src/kordle_helper/KordleHelperMain.js
@@ -9,27 +9,25 @@ import Header from './header/Header';
 function KordleHelperMain(params) {
 
     return (<div className={`KordleHelperMain`}>
-        <BrowserRouter>
-            <HelmetProvider>
-                <Helmet>
-                    <title>꼬들-도와줘!</title>
-                    <meta name="description" content="꼬들 & 꼬오오오오들 & 꼬맨틀 링크 및 헬프 페이지" />
-                </Helmet>
-                <Header />
-                {/* <LnbMenu /> */}
-                <Routes>
-                    <Route path="/" element={<Contents />}></Route>
-                    <Route path={GAME_INFO.LINK_URL[GAME_INFO.HELP]} element={<Contents />}></Route>
-                    <Route path={"/game/:urltype"} element={<GamePage />}></Route>
-                    <Route path="*" element={<Contents />}></Route>
-                </Routes>
-                
-                
-                {/* <Footer /> */}
-            </HelmetProvider>
-        </BrowserRouter>
-        
-
+            <BrowserRouter>
+                <HelmetProvider>
+                    <Helmet>
+                        <title>꼬들-도와줘!</title>
+                        <meta name="description" content="꼬들 & 꼬오오오오들 & 꼬맨틀 링크 및 헬프 페이지" />
+                    </Helmet>
+                    <Header />
+                    {/* <LnbMenu /> */}
+                    <Routes>
+                        <Route path="/" element={<Contents />}></Route>
+                        <Route path={GAME_INFO.LINK_URL[GAME_INFO.HELP]} element={<Contents />}></Route>
+                        <Route path={"/game/:urltype"} element={<GamePage />}></Route>
+                        <Route path="*" element={<Contents />}></Route>
+                    </Routes>
+                    
+                    
+                    {/* <Footer /> */}
+                </HelmetProvider>
+            </BrowserRouter>
         </div>) 
 }
 
